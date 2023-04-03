dff0879dd52acd45f872f2e08602fc2bac8e14ea
srestamalla <mallasresta@gmail.com>
2023-04-03 11:32:57 +0545

    minor fixes

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,16 @@ import Image from "next/image";
 import styles from "styles/Home.module.css";
 import FormComp from "@/components/form/FormComp";
 import FormFormik from "@/components/form/FormFormik";
+import AlternateSection from "@/components/alternatesection/AlternateSection";
+import FaqAccordian from "@/components/faqaccordian/FaqAccordian";
 
 export default function Home() {
   return (
     <>
-      <StaticSection />
-      <FormComp />
+      {/* <StaticSection />
+      <FormComp /> */}
+      <AlternateSection />
+      <FaqAccordian />
     </>
   );
 }
